0a2fb799f6a62dffd19f919a8f864964babc8168
rotimi-babalola <rotimi.babalola@andela.com>
2019-03-03 13:52:03 +0100

    add infinite scroll functionality to list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import List from './List';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col6 justify-content-center my-5">
+          <List />
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
